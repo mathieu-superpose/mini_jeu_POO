42aeb94d48ce76bb8aa4c1e7b86ed80020e3fd84
Mathieu <mathieu.joly@lrguas.ch>
2020-07-21 11:15:41 +0200

    up

--- app.rb
+++ app.rb
@@ -5,4 +5,28 @@ require 'pry'
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+player1 = Player.new("Josiane")
+player2 = HumanPlayer.new("José")
+
+puts "Voici l'état de chaque joueur :"
+puts player1.show_state
+puts player2.show_state
+puts "\n"
+
+puts "Passons à la phase d'attaque :"
+round = 1
+
+while player1.life_points > 0 || player2.life_points > 0
+	puts "Rounds #{round}"
+	puts player2.attacks(player1)
+	abort("Fin de la partie!") if (player1.life_points <= 0)
+	puts player1.attacks(player2)
+	abort("Fin de la partie!") if (player2.life_points <= 0)
+	puts "Fin du rounds #{round}, voici l'état de chaque joueur :"
+	puts player1.show_state
+	puts player2.show_state
+	puts "\n"
+	round += 1
+end
+
 binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -9,8 +9,8 @@ class Player
 
 	def initialize(name)
 		@name = name
-		@life_points = 10
-		puts self
+		@life_points = 50
+		#puts self
 	end#initialize
 	def show_state
 		return "#{name} a #{life_points} points de vie"
@@ -19,24 +19,41 @@ class Player
 		@life_points -= damage_points
 		"le joueur #{name} a été tué !" if life_points <= 0 
 	end#gets_damage(damage_points)
-	def attacks(player)
-		damage = compute_damage
-
-		puts player.name
-		
-		# case player
-		# when player1
-		# 	puts "le joueur player2 attaque le joueur player1"
-		# 	puts "il lui inflige #{damage} points de dommages"
-		# 	player2.gets_damage(damage_points)
-		# when player2
-		# 	puts "le joueur player1 attaque le joueur player2"
-		# 	puts "il lui inflige #{damage} points de dommages"
-		# 	player1.gets_damage(damage_points)
-		# end
-    end#attacks(player)
-    def compute_damage
+	def compute_damage
     	return rand(1..6)
   	end#compute_damage
+	def attacks(player)
+		damage = compute_damage
+		puts "#{@name} attaque #{player.name}"
+		puts "il lui inflige #{damage} points de dommages"
+		puts player.gets_damage(damage)
+    end#attacks(player)
+end#class Player
 
-end#class player
+class HumanPlayer < Player
+	attr_accessor :weapon_level
+
+	def initialize(name)
+		@name = name
+		@life_points = 100
+		@weapon_level = 1
+	end#initialize
+	def show_state
+		return "#{name} a #{life_points} points de vie et une arme de niveau #{weapon_level}"
+	end#show_state
+	def compute_damage
+    rand(1..6) * @weapon_level
+  	end#compute_damage
+  	def search_weapon
+  	level = rand(1..6)
+  	puts "Tu as trouvé une arme de niveau #{level}!"
+	  	if level > @weapon_level
+	  		puts"Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+	  		@weapon_level = level
+	  	else 
+	  		puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+	  	end
+  	end#search_weapon
+end#class HumanPlayer
+
+
